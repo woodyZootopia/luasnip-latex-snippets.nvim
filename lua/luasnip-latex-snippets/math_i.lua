1df28609a51aa89e02fa2b28b8e08f7fa7e8589d
woodyZootopia <piequalsabout314159@gmail.com>
2023-08-18 23:48:17 +0900

    Update

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -50,7 +50,7 @@ local math_i = {
   ),
   ls.parser.parse_snippet(
     { trig = "lr{", name = "left{ right}" },
-    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\\\} $0"
+    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\} $0"
   ),
   ls.parser.parse_snippet(
     { trig = "lr[", name = "left[ right]" },
